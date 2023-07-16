1bb28fd181e89fc23356bd3ddeb27d01acb76155
Dmytro Konstantinov <dmytro@ursa.dk>
2023-07-16 21:09:07 +0100

    Delete ssh_agent.sh

--- apkovl/etc/profile.d/ssh_agent.sh
+++
@@ -1,5 +0,0 @@
-ssh-agent-reuse() {
-    SSH_AUTH_SOCK="$(find /tmp/ssh-* -user ${USER} -maxdepth 1 -type s -name 'agent.*' -exec ls -1t {} + | head -1)"
-}
-
-[ -n "${SSH_AUTH_SOCK}" ] || ssh-agent-reuse
