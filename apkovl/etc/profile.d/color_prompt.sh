d261509c2c835e2d531dd4b13f7324bc6ff5a0e5
Dmytro Konstantinov <dmytro@ursa-bygrove.com>
2023-08-19 18:03:51 +0200

    Fixed associative array usage in ash

--- apkovl/etc/profile.d/color_prompt.sh
+++ apkovl/etc/profile.d/color_prompt.sh
@@ -3,27 +3,29 @@ printc() {
   : "${2:?Missing argument -- string to print}"
 
   # https://i.stack.imgur.com/utQ7mm.png
-  local -A colors=(
-    [black]='\e[1;30m'
-    [red]='\e[1;31m'
-    [green]='\e[1;32m'
-    [yellow]='\e[1;33m'
-    [blue]='\e[1;34m'
-    [magenta]='\e[1;35m'
-    [cyan]='\e[1;36m'
-    [lightgrey]='\e[1;37m'
-    [darkgrey]='\e[1;90m'
-    [lightred]='\e[1;91m'
-    [lightgreen]='\e[1;92m'
-    [lightyellow]='\e[1;93m'
-    [lightblue]='\e[1;94m'
-    [lightmagenta]='\e[1;95m'
-    [lightcyan]='\e[1;96m'
-    [white]='\e[1;97m'
-  )
+  colors() {
+    cat <<-EOS | grep -m 1 "${1}" | awk '{ printf "%s" $2 }'
+	black         \e[1;30m
+	red           \e[1;31m
+	green         \e[1;32m
+	yellow        \e[1;33m
+	blue          \e[1;34m
+	magenta       \e[1;35m
+	cyan          \e[1;36m
+	lightgrey     \e[1;37m
+	darkgrey      \e[1;90m
+	lightred      \e[1;91m
+	lightgreen    \e[1;92m
+	lightyellow   \e[1;93m
+	lightblue     \e[1;94m
+	lightmagenta  \e[1;95m
+	lightcyan     \e[1;96m
+	white         \e[1;97m
+	EOS
+  }
 
   local c="$(echo "${1}" | tr -d '[[:space:]]_-' | tr '[[:upper:]]' '[[:lower:]]')"
-  printf '%s%s\e[0m' "${colors[$c]}" "${2}";
+  printf "$(colors $c)%s\e[0m" "${2}";
 }
 
 color_prompt() {
@@ -55,4 +57,4 @@ color_prompt() {
   PS1="${user}@${host}:\w \$ "
 }
 
-color_prompt
+# color_prompt
