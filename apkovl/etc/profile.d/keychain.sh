ee8dc95c10a72685fef5eb3402850950b68d19ed
Dmytro Konstantinov <dmytro@ursa-bygrove.com>
2023-08-21 18:32:39 +0200

    Only run keychain if the command is installed

--- apkovl/etc/profile.d/keychain.sh
+++ apkovl/etc/profile.d/keychain.sh
@@ -1,7 +1,9 @@
-keychain \
-  --absolute \
-  --dir "${XDG_STATE_HOME}/keychain" \
-  --agents ssh \
-  --inherit any-once \
-  --stop others
-. "${XDG_STATE_HOME}/keychain/${HOSTNAME}-sh"
+if type -P keychain > /dev/null; then
+  keychain \
+    --absolute \
+    --dir "${XDG_STATE_HOME}/keychain" \
+    --agents ssh \
+    --inherit any-once \
+    --stop others
+  . "${XDG_STATE_HOME}/keychain/${HOSTNAME}-sh"
+fi
